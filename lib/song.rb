f9d8cdb4355c4dd8571cbea897dc4be776b1a3b9
kkramarczuk11 <kkramarczuk11@gmail.com>
2018-06-22 12:59:48 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,27 +26,27 @@ class Song
     @@genres.uniq
   end
 
-  def self.genre_count
-    genre_count = {}
-    @@genres.each do |genre|
-      if genre_count[genre]
-       genre_count[genre] += 1    #sees genre is not in the hash, then it will add it
-     else
-       genre_count[genre] = 1     #sees genre IS in the hash, will not add it
-     end
-   end
-   genre_count
- end
-
-  def self.artist_count
-    artist_count = {}
-    @@artists.each do |artist|
-      if artist_count[artist]
-       artist_count[artist] += 1    #sees artist is not in the hash, then it will add it
-     else
-       artist_count[artist] = 1     #sees artist IS in the hash, will not add it
-     end
-   end
-   artist_count
- end
+ #  def self.genre_count
+ #    genre_count = {}
+ #    @@genres.each do |genre|
+ #      if genre_count[genre]
+ #       genre_count[genre] += 1    #sees genre is not in the hash, then it will add it
+ #     else
+ #       genre_count[genre] = 1     #sees genre IS in the hash, will not add it
+ #     end
+ #   end
+ #   genre_count
+ # end
+ # 
+ #  def self.artist_count
+ #    artist_count = {}
+ #    @@artists.each do |artist|
+ #      if artist_count[artist]
+ #       artist_count[artist] += 1    #sees artist is not in the hash, then it will add it
+ #     else
+ #       artist_count[artist] = 1     #sees artist IS in the hash, will not add it
+ #     end
+ #   end
+ #   artist_count
+ # end
  end
