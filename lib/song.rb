41ad02884fb8ce609a02f6e4fb6a54707f9b5787
kkramarczuk11 <kkramarczuk11@gmail.com>
2018-06-22 13:09:38 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,7 @@
 class Song
   attr_accessor :name, :artist, :genre
 
-  # @@count = 0           #class variables need to be initialized so that the computer knows that class variable is a variable
+  @@count = 0           #class variables need to be initialized so that the computer knows that class variable is a variable
   @@artists = []
   @@genres = []
 
@@ -9,7 +9,7 @@ class Song
     @name = name
     @artist = artist
     @genre = genre
-    # @@count += 1
+    @@count += 1
     @@artists << artist     #make sure students spell things correctly
     @@genres << genre
   end
@@ -26,27 +26,27 @@ class Song
     @@genres.uniq
   end
 
- #  def self.genre_count
- #    genre_count = {}
- #    @@genres.each do |genre|
- #      if genre_count[genre]
- #       genre_count[genre] += 1    #sees genre is not in the hash, then it will add it
- #     else
- #       genre_count[genre] = 1     #sees genre IS in the hash, will not add it
- #     end
- #   end
- #   genre_count
- # end
- #
- #  def self.artist_count
- #    artist_count = {}
- #    @@artists.each do |artist|
- #      if artist_count[artist]
- #       artist_count[artist] += 1    #sees artist is not in the hash, then it will add it
- #     else
- #       artist_count[artist] = 1     #sees artist IS in the hash, will not add it
- #     end
- #   end
- #   artist_count
- # end
+  def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre]
+       genre_count[genre] += 1    #sees genre is IN hash, then counts the songs
+     else
+       genre_count[genre] = 1     #sees genre IS NOT in the hash, will not add it
+     end
+   end
+   genre_count
+ end
+ 
+  def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist]
+       artist_count[artist] += 1    #sees artist is not in the hash, then it will add it
+     else
+       artist_count[artist] = 1     #sees artist IS in the hash, will not add it
+     end
+   end
+   artist_count
+ end
  end
